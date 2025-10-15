5735151612b62ef54bcd497d4f4c3dd41bce9205
GiacomoVisconti <giacomovisconti1999@gmail.com>
2025-10-15 12:51:19 +0200

    Add search functionality for pizzas by name

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controllers/PizzaController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controllers/PizzaController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -20,7 +21,7 @@ public class PizzaController {
     public String index(Model model) {
 
         model.addAttribute("list", repository.findAll());
-
+        
         return "pizzas/index";
     }
 
@@ -30,6 +31,15 @@ public class PizzaController {
         model.addAttribute("pizza", repository.findById(id).get());
 
         return "pizzas/pizzadetail";
+    }
+
+    @GetMapping("/searchByName")
+    public String searchByName(@RequestParam(name="name") String name, Model model){
+
+        model.addAttribute("list", repository.findByNameContaining(name));
+
+        
+        return "pizzas/index";
     }
     
 }
