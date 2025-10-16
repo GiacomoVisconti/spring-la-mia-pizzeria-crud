b5952f4f764ac2dd55346194e8da0e9cb9ad1d9a
GiacomoVisconti <giacomovisconti1999@gmail.com>
2025-10-16 11:54:39 +0200

    add update record fiture

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controllers/PizzaController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controllers/PizzaController.java
@@ -64,6 +64,22 @@ public class PizzaController {
         repository.save(formPizza);
         return "redirect:/pizzas/";
     }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id,  Model model) {
+        model.addAttribute("pizza", repository.findById(id).get());
+        return "pizzas/editPizza";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String update(@Valid @ModelAttribute("pizza") Pizza formPizza,
+    BindingResult bindingResult, Model model ) {
+        if (bindingResult.hasErrors()) {
+            return"pizzas/editPizza";
+        }
+        repository.save(formPizza);
+        return "redirect:/pizzas/";
+    }
     
     
 }
